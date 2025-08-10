017d1651e7c12a03fde33cc1b2496009d952628f
mazenaliii <125923701+mazenaliii@users.noreply.github.com>
2025-08-10 04:29:01 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <style jsx global>{`
+        html, body {
+          margin: 0;
+          padding: 0;
+          overflow-x: hidden; /* Prevent horizontal overflow */
+        }
+        * {
+          box-sizing: border-box; /* Ensure consistent box model */
+        }
+      `}</style>
       <body className="bg-[#1D1104]">
         <Navbar />
         {children}
